fab7360b1cf5c2c5ef0116d83f1fc398f9b2909b
emibonezzi <emibonezzi@gmail.com>
2024-05-08 08:55:42 -0400

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div
       id="container"
-      className="snap-mandatory h-[100dvh] overflow-y-scroll snap-y scroll-smooth"
+      className="snap-mandatory h-screen overflow-y-scroll snap-y scroll-smooth"
     >
       <Hero />
       <Intro />
