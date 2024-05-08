4dd67f1fcb46283a361ba44b1c05fb80592f8ad8
emibonezzi <emibonezzi@gmail.com>
2024-05-08 11:04:25 -0400

    Fix contacts

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -2,24 +2,26 @@ const Contacts = () => {
   return (
     <section
       id="contacts"
-      className="h-[100dvh] relative snap-center px-3 lg:pl-10 grid grid-cols-[2fr_1fr] justify-center items-center"
+      className="h-[100dvh]  snap-center grid grid-cols-[280px] lg:grid-cols-[700px] justify-center items-center"
     >
       <div>
-        <h1 className="text-[2.5em] lg:text-[7.5rem] tracking-tight lg:tracking-tighter font-extrabold">
+        <h1 className="text-[3em] lg:text-[7.5rem] tracking-tight lg:tracking-tighter font-extrabold">
           Let's connect
         </h1>
-        <p className="text-lg lg:text-2xl font-light">
-          Reach out via email
-          <br />
-          <span className="underline">
-            <a href="mailto:me@emilianobonezzi.com">me@emilianobonezzi.com</a>
-          </span>
-        </p>
+        <div className="flex gap-3 lg:justify-between items-center">
+          <img
+            className="rounded-full w-[80px] lg:w-[100px] border-[1px] border-black"
+            src="https://media.licdn.com/dms/image/D4D03AQFVX-RWjIIyjA/profile-displayphoto-shrink_800_800/0/1698223282520?e=1720051200&v=beta&t=tzYp6V4oYhjZ8qhpmG-x_oIl86UtsR8fjULAWxUQY9E"
+          />
+          <p className="text-lg lg:text-2xl font-light">
+            Reach out via email
+            <br />
+            <span className="underline">
+              <a href="mailto:me@emilianobonezzi.com">me@emilianobonezzi.com</a>
+            </span>
+          </p>
+        </div>
       </div>
-      <img
-        className="rounded-full w-[290px] border-[1px] p-1 border-black"
-        src="https://media.licdn.com/dms/image/D4D03AQFVX-RWjIIyjA/profile-displayphoto-shrink_800_800/0/1698223282520?e=1720051200&v=beta&t=tzYp6V4oYhjZ8qhpmG-x_oIl86UtsR8fjULAWxUQY9E"
-      />
     </section>
   );
 };
